cf089f4f84e94833dad756cac0359f6d4d347c47
wallacesw11 <wallace.dsn.erp@gmail.com>
2020-09-27 19:31:19 -0300

    adicionando o banco postgres

--- src/Controller/HomeController.js
+++ src/Controller/HomeController.js
@@ -1,10 +1,15 @@
+const connection = require('../database/connection');
+
+
 const respostaHome = {
   company: "WallTerdata",
   message: "Bem vindo ao Backend do WallVendas!!! Para mais informações, acesse www.wallvendas.com"
 };
 
 module.exports = {
-  home(request, response) {
-    return response.status(400).json(respostaHome);
+  async home(request, response) {
+    const produto = await connection('produto').select('*');
+
+    return response.status(400).json(produto);
   }
 }
