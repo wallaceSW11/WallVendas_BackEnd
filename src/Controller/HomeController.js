d84fb192b2f6babe53a3ee58d9cbb38e9a367f3b
wallacesw11 <wallace.dsn.erp@gmail.com>
2020-09-27 16:59:56 -0300

    atualizando o home

--- src/Controller/HomeController.js
+++ src/Controller/HomeController.js
@@ -1,5 +1,10 @@
+const respostaHome = {
+  company: "WallTerdata",
+  message: "Bem vindo ao Backend do WallVendas!!! Para mais informações, acesse www.wallvendas.com"
+};
+
 module.exports = {
   home(request, response) {
-    return response.json("hello world");
+    return response.status(400).json(respostaHome);
   }
 }
