fc29560cb9687e28bd82d81589df9e3732d8cc4d
wallacesw11 <wallace.dsn.erp@gmail.com>
2020-09-30 18:48:34 -0300

    testando alterações para o migrate

--- knexfile.js
+++ knexfile.js
@@ -24,31 +24,42 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    dialect: 'postgres',
-    connection: {
-      host: 'ec2-34-236-215-156.compute-1.amazonaws.com',
-      database: 'd6v2k843oh96ij',
-      user: 'kzjjcojpbmretv',
-      password: 'fd924c24b89b706b88ee9af0bbd27c48d70c1c9f517af29c691048ee6fba0a17',
-      // ssl: true,
-      // dialectOptions: {
-      //   "ssl": {
-      //     require: true,
-      //     rejectUnauthorized: true
-      //   }
-      // }
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './src/database/migrations'
+      directory: './database/migrations',
     },
-    useNullAsDefault: true,
-  },
+    seeds: { directory: './database/seeds' },
+  }
+
+  // production: {
+  //   client: 'postgresql',
+  //   dialect: 'postgres',
+  //   connection: process.env.DATABASE_URL,
+  //   // connection: {
+  //   //   database: process.env.DATABASE_URL,
+  //   //   // host: 'ec2-34-236-215-156.compute-1.amazonaws.com',
+  //   //   // database: 'd6v2k843oh96ij',
+  //   //   // user: 'kzjjcojpbmretv',
+  //   //   // password: 'fd924c24b89b706b88ee9af0bbd27c48d70c1c9f517af29c691048ee6fba0a17',
+  //   //   // ssl: true,
+  //   //   // dialectOptions: {
+  //   //   //   "ssl": {
+  //   //   //     require: true,
+  //   //   //     rejectUnauthorized: true
+  //   //   //   }
+  //   //   // }
+  //   // },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //     directory: './src/database/migrations'
+  //   },
+  //   useNullAsDefault: true,
+  // },
 
   staging: {
     client: 'postgresql',
